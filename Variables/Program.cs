c1ab89e22de0c9558d1b5c14448f52b1fd1f239d
Tanner Van Amen <Tvanamen@yahoo.com>
2022-06-28 17:29:55 -0600

    inital commit

---
+++ Variables/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+string ?myDog = "Loki";
+int myDogAge = 1;
+bool hairFluffy = true;
+decimal dogFood = 2.5m;
+double dogWeight = 47.85;
+    Console.WriteLine($"My dogs name is{ myDog} He is{ myDogAge} Years old");
+    Console.WriteLine($"Does {myDog} have fluffy hair {hairFluffy}");
+Console.WriteLine($"{myDog} eats {dogFood} cups of food each day and weighs {dogWeight} pounds");
+
+
